6de43bc093b79a3ca94bad1518a912203815ceba
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-14 14:35:30 +0700

    Add new file and project. Create object and various properties that include strings, booleans,number and arrays. Want to add another object as a object property later on.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const strain1= {
+    name: "Strawberry Cough",
+type: "Sativa",
+cannabinoids: ["THC: 21%", "CBG: 1%"],
+    terpines:["Myrcene", "Pinene", "Caryophyllene"],
+    flavor: "Sweet",
+inStock: false,
+}
+console.log(strain1);
+strain1.flavor = ["Sweet", "Strawberry", "Berries"]
+console.log(strain1.flavor)
