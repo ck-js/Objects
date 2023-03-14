587ad610212e0c6cb0fa982cb3a0da59baa57e53
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-14 16:40:03 +0700

    Add an object method to our strain1 object, an object method is basically a function inside of an object that can be called outside of the object and can include parameteres and return statements inside its code block

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ cannabinoids: ["THC: 21%", "CBG: 1%"],
     terpines:["Myrcene", "Pinene", "Caryophyllene"],
     flavor: "Sweet",
 inStock: false,
+talkingPoints: function(word) {
+    console.log("Energetic, creative" + word);
+}
 }
 console.log(strain1);
 strain1.flavor = ["Sweet", "Strawberry", "Berries"]
 console.log(strain1.flavor)
+
+strain1.talkingPoints(" and uplifting");
